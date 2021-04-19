0952ce3e16fbc26c7297619ace935316cbbc3efd
LiuSandy <nsiliushuang@gmail.com>
2021-04-19 14:03:40 +0800

    modify:remove element

--- algorithm/src/RemoveElement/index.js
+++ algorithm/src/RemoveElement/index.js
@@ -1,21 +1,36 @@
-export default function (nums, val) {
-    let curentIndex = 0
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== val) {
-            nums[curentIndex] = nums[i]
-            curentIndex++
-        }
+// export default function (nums, val) {
+//     let curentIndex = 0
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] !== val) {
+//             nums[curentIndex] = nums[i]
+//             curentIndex++
+//         }
+//     }
+//     return curentIndex
+// }
+
+// /**
+//  * 使用 splice
+//  */
+// export function removeElement(nums, val) {
+//     while (nums.includes(val)) { 
+//         nums.splice(nums.indexOf(val, 0), 1); 
+//     }
+//     return nums.length
+// }
+
+const removeElement = (nums, val) => {
+  let left = 0, right = nums.length;
+  while (left < right) {
+    if (nums[left] === val) {
+      nums[left] = nums[right - 1]
+      right--
+    } else {
+      left++
     }
-    return curentIndex
+  }
+  return left
 }
 
-/**
- * 使用 splice
- */
-export function removeElement(nums, val) {
-    while (nums.includes(val)) { 
-        nums.splice(nums.indexOf(val, 0), 1); 
-    }
-    return nums.length
-}
-
+const result = removeElement([3, 2, 2, 3], 2)
+console.log(result)
