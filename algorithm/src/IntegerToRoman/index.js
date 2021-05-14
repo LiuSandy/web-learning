0e824ff464cf2a3fb66bff2b3f94f146c571fd71
LiuSandy <nsiliushuang@gmail.com>
2021-05-14 19:53:17 +0800

    add romanb

--- algorithm/src/IntegerToRoman/index.js
+++ algorithm/src/IntegerToRoman/index.js
@@ -1,14 +1,33 @@
-export default function(num){
-  const map = {
-    1:'I',
-    5:'V',
-    10:'X',
-    50:'L',
-    100:'C',
-    500:'D',
-    1000:'M',
+/**
+ * 12. Integer to Roman
+ * 12. 整数转罗马数字
+ */
+
+const intToRoman = function (num) {
+
+  const list = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1],
+  ]
+  const result = []
+  for (const [key, value] of list) {
+    while (num >= value && num > 0) {
+      num -= value
+      result.push(key)
+    }
   }
-  const keys = Object.keys(map)
-  console.log(keys);
-  
-}
+  return result.join("")
+};
+const result = intToRoman(4)
+console.log(result)
