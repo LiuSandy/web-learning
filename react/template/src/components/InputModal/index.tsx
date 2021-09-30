d060be49d48405f9ede5e08efeb4bce91f9ffe48
LiuSandy <nsiliushuang@gmail.com>
2021-09-30 16:57:25 +0800

    feat: 下拉选择为受控

--- react/template/src/components/InputModal/index.tsx
+++ react/template/src/components/InputModal/index.tsx
@@ -2,9 +2,10 @@
  * Input + Select + Modal
  * 适用场景，输入时自动联想已存在内容，也可以通过对话框的方式进行搜索
  */
-import React from 'react';
+import React, { useCallback } from 'react';
 import { AutoComplete, Input, Modal } from 'antd';
 import type { AutoCompleteProps, ModalProps } from 'antd';
+import { useBoolean } from 'ahooks';
 
 interface IProps extends Omit<AutoCompleteProps, 'onChange'> {
   options?: any[];
@@ -22,8 +23,12 @@ interface IProps extends Omit<AutoCompleteProps, 'onChange'> {
 
 const Index: React.FC<IProps> = (props: IProps) => {
   const { options, value, children, onChange, modalProps, onModalSearch, ...rest } = props;
-
+  const [open, { toggle }] = useBoolean(false);
   const selectLabel = options?.find((item) => item.value === value)?.label || '';
+
+  const handleOpenChange = useCallback(() => {
+    toggle();
+  }, [toggle]);
 
   return (
     <>
@@ -33,9 +38,17 @@ const Index: React.FC<IProps> = (props: IProps) => {
         options={options}
         placeholder="请输入"
         onChange={onChange}
+        onBlur={handleOpenChange}
+        onFocus={handleOpenChange}
+        open={open}
         {...rest}
       >
-        <Input.Search onSearch={onModalSearch} />
+        <Input.Search
+          onSearch={(v, e) => {
+            e?.stopPropagation();
+            onModalSearch?.();
+          }}
+        />
       </AutoComplete>
       <Modal footer={null} {...modalProps} {...rest}>
         {children}
