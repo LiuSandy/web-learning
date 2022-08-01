d96b3cfbbc7db2c9a6fd2059e3b847ee01311c08
LiuSandy <nsiliushuang@gmail.com>
2022-08-01 23:02:59 +0800

    Three Tutoroal 03: animation

---
+++ threejs-tutorial/animations/src/script.js
@@ -0,0 +1,53 @@
+import './style.css'
+import * as THREE from 'three'
+import gsap from 'gsap';
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+
+// option 1: use Clock
+const clock = new THREE.Clock();
+
+// option 2: use gsap library
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 });
+
+// Use window.requestAnimationFrame()
+const tick = () => {
+
+    // const elapsed = clock.getElapsedTime();
+    // mesh.position.x = Math.sin(elapsed);
+    // mesh.position.y = Math.sin(elapsed);
+
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(tick)
+}
+
+tick()
